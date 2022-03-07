406637ce86971ee6b1fbb5f37cf387c74610cadb
CalvinChang <q20274982@gamil.com>
2022-03-07 15:39:26 +0800

    新增自訂卡片區塊

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const readCookieValueInput = document.querySelector('#read_cookie_value')
 const createBtn = document.querySelector('.create-block button')
 const readBtn = document.querySelector('.read-block button')
 
+const handonModelInput = document.querySelector('#handon_model')
+const handJsObjectInput = document.querySelector('#handon_js_object')
+const handJsJsonInput = document.querySelector('#handon_js_json')
+
+const cardRenderScope = document.querySelector('.cards-block .row')
+
 function createCookie () {
   const key = createCookieKeyInput.value
   const value = createCookieValueInput.value
@@ -24,3 +30,36 @@ function readCookie () {
 
 createBtn.addEventListener('click', () => { createCookie() })
 readBtn.addEventListener('click', () => { readCookie() })
+
+handonModelInput.value =
+`[{
+  name: '商品名稱',
+  imgUrl: '商品連結',
+  content: '商品內文'
+}]`
+
+function covertStringToObject () {
+  return eval(handJsObjectInput.value)
+}
+
+handJsObjectInput.addEventListener('input', () => {
+  handJsJsonInput.value = JSON.stringify(covertStringToObject(), null, 4)
+
+  cardRenderScope.innerHTML = ''
+  
+  covertStringToObject().forEach(el => {
+    cardRenderScope.innerHTML +=
+    `
+    <div class="col-3">
+      <div class="card" style="width: 18rem;">
+        <img alt="" src="${el.imgUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${el.name}</h5>
+          <p class="card-text">${el.content}</p>
+        </div>
+      </div>
+    </div>
+    `
+  })
+
+})
